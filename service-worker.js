2d4a94e5b9524a653efe67903d9b459509d1c6b2
Duncan <duncanmarten@yahoo.com>
2021-01-20 20:34:07 -0600

    added manifest to service worker

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,8 @@ const FILES_TO_CACHE = [
     "./dist/app.bundle.js",
     "./dist/events.bundle.js",
     "./dist/tickets.bundle.js",
-    "./dist/schedule.bundle.js"
+    "./dist/schedule.bundle.js",
+    "./dist/manifest.json"
 ];
 
 self.addEventListener('fetch', function (e) {
